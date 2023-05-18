c8f5db99ed2f451df6f8c88df5bd3a0ff5d85510
Karly <karlyrosinsky@gmail.com>
2023-05-18 14:56:10 -0500

    Fixed up the layout and pages file so the build would run

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,48 @@
+import React from "react";
+import { Karla, DM_Serif_Display } from "next/font/google";
+
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import Different from "@/components/Different";
 import FindOut from "@/components/FindOut";
 import Footer from "@/components/Footer";
 
-export default function Home() {
+import "./globals.css";
+
+const karla = Karla({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-karla",
+});
+
+const dm_serif_display = DM_Serif_Display({
+  weight: "400",
+  display: "swap",
+  subsets: ["latin"],
+  variable: "--font-dm-serif-display",
+});
+
+const metadata = {
+  title: "Insure landing page",
+  description: "Mockup of an Insure company landing page by create next app",
+};
+
+const Home: React.FC = () => {
   return (
-    <main>
-      <Navbar></Navbar>
-      <Hero></Hero>
-      <Different></Different>
-      <FindOut></FindOut>
-      <Footer></Footer>
-    </main>
+    <html
+      lang="en"
+      className={`${karla.variable} ${dm_serif_display.variable}`}
+    >
+      <body>
+        <Navbar />
+        <Hero />
+        <Different />
+        <FindOut />
+        <Footer />
+      </body>
+    </html>
   );
-}
+};
+
+export default Home;
